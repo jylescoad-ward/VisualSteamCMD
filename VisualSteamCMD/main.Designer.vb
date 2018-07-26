ed60bef45b9fde8b4c68c5221accf49383dbec67
Jyles Coad-Ward <github@jylescoad-ward.com>
2018-07-26 16:44:40 +0800

    FIRST COMMIT
    A Simple Visual SteamCMD for installing demos and deticated servers.
    Was coded in VB.NET on Visual Studio 2017 Community.
    
    POWERED BY jyles.pw

---
+++ VisualSteamCMD/main.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.InstallLocation = New System.Windows.Forms.Button()
+        Me.DownloadSteamCMD = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SetGameID = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Install = New System.Windows.Forms.Button()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.InstallLocationDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.FirstRun = New System.Windows.Forms.Button()
+        Me.FolderLocationDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'InstallLocation
+        '
+        Me.InstallLocation.Location = New System.Drawing.Point(232, 196)
+        Me.InstallLocation.Name = "InstallLocation"
+        Me.InstallLocation.Size = New System.Drawing.Size(139, 23)
+        Me.InstallLocation.TabIndex = 2
+        Me.InstallLocation.Text = "Install Location"
+        Me.InstallLocation.UseVisualStyleBackColor = True
+        '
+        'DownloadSteamCMD
+        '
+        Me.DownloadSteamCMD.Location = New System.Drawing.Point(232, 110)
+        Me.DownloadSteamCMD.Name = "DownloadSteamCMD"
+        Me.DownloadSteamCMD.Size = New System.Drawing.Size(139, 23)
+        Me.DownloadSteamCMD.TabIndex = 4
+        Me.DownloadSteamCMD.Text = "Download SteamCMD"
+        Me.DownloadSteamCMD.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Step 1."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 176)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Step 2."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 201)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Set Server Install Location"
+        '
+        'SetGameID
+        '
+        Me.SetGameID.Location = New System.Drawing.Point(232, 298)
+        Me.SetGameID.Name = "SetGameID"
+        Me.SetGameID.Size = New System.Drawing.Size(139, 23)
+        Me.SetGameID.TabIndex = 8
+        Me.SetGameID.Text = "Set GameID"
+        Me.SetGameID.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(13, 245)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Step 3."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 265)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Set GameID"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(14, 290)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(123, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Deticated Server ID List:"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(14, 303)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(77, 13)
+        Me.LinkLabel1.TabIndex = 12
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = ">>click here<<"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(-11, 222)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(547, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "_________________________________________________________________________________" &
+    "_________"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(-34, 324)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(547, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "_________________________________________________________________________________" &
+    "_________"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(-55, 149)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(547, 13)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "_________________________________________________________________________________" &
+    "_________"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(-55, 95)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(547, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "_________________________________________________________________________________" &
+    "_________"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(5, 9)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(278, 37)
+        Me.Label11.TabIndex = 17
+        Me.Label11.Text = "Visual SteamCMD"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(230, 46)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(141, 13)
+        Me.Label12.TabIndex = 18
+        Me.Label12.Text = "Created by Jyles Coad-Ward"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(195, 59)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(176, 13)
+        Me.Label13.TabIndex = 19
+        Me.Label13.Text = "Copyright 2017-2018 http://jyles.pw"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(9, 133)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(112, 13)
+        Me.Label14.TabIndex = 21
+        Me.Label14.Text = "Download SteamCMD"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(11, 361)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(221, 20)
+        Me.Label15.TabIndex = 22
+        Me.Label15.Text = "Start And Install Game/Server"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label16.Location = New System.Drawing.Point(13, 337)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(63, 24)
+        Me.Label16.TabIndex = 23
+        Me.Label16.Text = "Step 4"
+        '
+        'Install
+        '
+        Me.Install.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Install.Location = New System.Drawing.Point(12, 384)
+        Me.Install.Name = "Install"
+        Me.Install.Size = New System.Drawing.Size(359, 44)
+        Me.Install.TabIndex = 24
+        Me.Install.Text = "Install Game/Server"
+        Me.Install.UseVisualStyleBackColor = True
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(9, 146)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(191, 13)
+        Me.Label17.TabIndex = 25
+        Me.Label17.Text = "(Skip this if you have already done this)"
+        '
+        'InstallLocationDialog
+        '
+        Me.InstallLocationDialog.FileName = "InstallLocationDialog"
+        '
+        'FirstRun
+        '
+        Me.FirstRun.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FirstRun.Location = New System.Drawing.Point(232, 133)
+        Me.FirstRun.Name = "FirstRun"
+        Me.FirstRun.Size = New System.Drawing.Size(139, 23)
+        Me.FirstRun.TabIndex = 26
+        Me.FirstRun.Text = "First Run (REQUIRED)"
+        Me.FirstRun.UseVisualStyleBackColor = True
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 440)
+        Me.Controls.Add(Me.FirstRun)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Install)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.SetGameID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DownloadSteamCMD)
+        Me.Controls.Add(Me.InstallLocation)
+        Me.Name = "main"
+        Me.Text = "VisualSteamCMD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents InstallLocation As Button
+    Friend WithEvents DownloadSteamCMD As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents SetGameID As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Install As Button
+    Friend WithEvents Label17 As Label
+    Friend WithEvents InstallLocationDialog As OpenFileDialog
+    Friend WithEvents FirstRun As Button
+    Friend WithEvents FolderLocationDialog As FolderBrowserDialog
+End Class
---
+++ VisualSteamCMD/main.vb
@@ -0,0 +1,64 @@
+﻿Public Class main
+    Public gameid As String = "nul"
+    Public installfolder As String = "nul"
+    Public steamcmd As String = "C:\steamcmd\steamcmd.exe"
+    Private Sub DownloadSteamCMD_Click(sender As Object, e As EventArgs) Handles DownloadSteamCMD.Click
+        If (Not System.IO.Directory.Exists("C:\steamcmd")) Then
+            System.IO.Directory.CreateDirectory("C:\steamcmd")
+        End If
+        MessageBox.Show("Created SteamCMD Folder at Location: C:\SteamCMD\")
+        My.Computer.Network.DownloadFile(
+    "http://jyles.pw/steamcmd.exe",
+    "C:\steamcmd\steamcmd.exe")
+        MessageBox.Show("Downloaded SteamCMD EXE to: C:\SteamCMD\SteamCME.exe")
+    End Sub
+
+    Private Sub FirstRun_Click(sender As Object, e As EventArgs) Handles FirstRun.Click
+        Dim proc As New System.Diagnostics.Process()
+        MessageBox.Show("A Window will open up, Dont worry! This will install SteamCMD, Do NOT close the window. It will Close itself.")
+
+        'First Start of SteamCMD to Update'
+        Dim firstStart As New ProcessStartInfo
+        firstStart.FileName = steamcmd
+        firstStart.Arguments = "+quit"
+        Process.Start(firstStart)
+
+        MessageBox.Show("SteamCMD has finished installing, it should of installed by now and closed itself. You can now continue to Step 2.")
+    End Sub
+
+    Private Sub InstallLocation_Click(sender As Object, e As EventArgs) Handles InstallLocation.Click
+        Dim folderDlg As New FolderBrowserDialog
+
+        folderDlg.ShowNewFolderButton = True
+
+        If (folderDlg.ShowDialog() = DialogResult.OK) Then
+
+            installfolder = folderDlg.SelectedPath
+
+            Dim root As Environment.SpecialFolder = folderDlg.RootFolder
+
+        End If
+        MessageBox.Show(installfolder)
+    End Sub
+
+    Private Sub SetGameID_Click(sender As Object, e As EventArgs) Handles SetGameID.Click
+        gameid = InputBox("Enter SteamCMD GameID from the Link", "SteamCMD GameID Input", "Enter GameID From List")
+
+        MessageBox.Show("gameid set to: " & gameid)
+
+    End Sub
+
+    Private Sub Install_Click(sender As Object, e As EventArgs) Handles Install.Click
+        Dim startInfo As New ProcessStartInfo
+        startInfo.FileName = "C:\steamcmd\steamcmd.exe"
+        startInfo.Arguments = "+login anonymous +force_install_dir " & installfolder & " +app_update " & gameid & " validate +quit"
+        Process.Start(startInfo)
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim showWeb As New ProcessStartInfo
+        showWeb.FileName = "iexplore.exe"
+        showWeb.Arguments = "https://developer.valvesoftware.com/wiki/Dedicated_Servers_List"
+        Process.Start(showWeb)
+    End Sub
+End Class
